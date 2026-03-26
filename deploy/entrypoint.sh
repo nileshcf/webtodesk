1f87b25bd6381e4f385f4e0b3c9aa6ed3385eeec
nileshcf <nilesh.verma@cloudframe.com>
2026-03-26 20:28:03 +0530

    added github - deployment startegy

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+set -eu
+
+start_jar() {
+  jar_glob="$1"
+  port="$2"
+  name="$3"
+
+  # shellcheck disable=SC2086
+  java -jar $jar_glob --server.port="$port" &
+  echo "Started $name (port $port) PID=$!"
+}
+
+echo "Starting services..."
+
+# Start discovery first
+start_jar "/app/discovery/*.jar" "8761" "discovery"
+sleep 3
+
+# Start user + conversion
+start_jar "/app/user/*.jar" "8081" "user"
+start_jar "/app/conversion/*.jar" "8082" "conversion"
+sleep 3
+
+# Start gateway last
+start_jar "/app/gateway/*.jar" "8080" "gateway"
+
+echo "Starting nginx..."
+nginx -g "daemon off;"
+
